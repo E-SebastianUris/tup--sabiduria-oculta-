084b743d0a74de2aaf1ef08aeaaf239e6c081c51
Melany Besguro <MelanyBesguro@outlook.com>
2026-04-20 20:19:26 -0300

    Issue 0 First commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      {/* El HTML queda vacío según la consigna, pero listo para ejecutar */}
+      <main>
+        <h1>Proyecto Base</h1>
+      </main>
+    </>
+  )
+}
+
+export default App
